5b88fa7ab8128cbe5be05e27c955e2e3b65adff2
arrkiin <arrkiin@gmail.com>
2018-06-26 12:52:17 +0200

    Links in markdown handle local and external links

--- gatsby/src/components/Link/Plain.js
+++ gatsby/src/components/Link/Plain.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { navigateTo } from 'gatsby-link';
 import { Text, Linking } from 'react-native';
+
+const openURL = url => {
+  const internal = /^\/(?!\/)/.test(url);
+  if (internal) {
+    navigateTo(url);
+  } else {
+    Linking.openURL(url);
+  }
+};
 
 const LinkNormal = props => (
   <Text
     {...props}
-    onPress={() => Linking.openURL(props.href)}
+    onPress={() => openURL(props.href)}
     style={{ textDecorationLine: 'underline' }}
   />
 );
